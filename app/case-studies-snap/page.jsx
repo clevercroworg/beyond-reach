a003b9dae5b4b9beb40917e77b7bd1f0dba05c41
sanjay kumar <sanjayconnecting007@gmail.com>
2026-05-31 12:23:36 +0530

    Refine case studies layout: make web card match mobile aspect ratio (width 600px, image height 340px), remove menu symbol from navbar, and make navbar background transparent

--- app/case-studies-snap/page.jsx
+++ app/case-studies-snap/page.jsx
@@ -209,20 +209,6 @@ export default function CaseStudiesSnapPage() {
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
             </svg>
           </a>
-          
-          <button className={styles.menuBtn} onClick={() => {
-            // Anchor to main menu triggers if available in layout wrapper
-            const globalMenuBtn = document.querySelector('button[class*="menuBtn"]');
-            if (globalMenuBtn) {
-              globalMenuBtn.click();
-            } else {
-              window.location.href = '/contact';
-            }
-          }}>
-            <span className={styles.hamburgerLine}></span>
-            <span className={styles.hamburgerLine}></span>
-            <span className={styles.hamburgerLine}></span>
-          </button>
         </div>
       </nav>
 
